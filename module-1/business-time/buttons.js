722b650c030469688aff50f2425bd05087dfe609
Marcus Radder <marcusradder@gmail.com>
2021-03-26 00:58:46 -0700

    Adding basic html and styles for my assignments.

---
+++ module-1/business-time/buttons.js
@@ -0,0 +1,33 @@
+var customItem1 = document.getElementById("customItem1");
+var customItem2 = document.getElementById("customItem2");
+var customItem3 = document.getElementById("customItem3");
+
+
+function showCustom2() {
+    customItem1.style.display = "none"
+    customItem2.style.display = "flex"
+    customItem3.style.display = "none"
+    console.log("go to 2")
+}
+
+function showCustom3() {
+    customItem1.style.display = "none"
+    customItem2.style.display = "none"
+    customItem3.style.display = "flex"
+    console.log("go to 3")
+
+}
+
+
+function showCustom1() {
+    customItem1.style.display = "flex"
+    customItem2.style.display = "none"
+    customItem3.style.display = "none"
+    console.log("go to 1")
+
+}
+
+function showHamburgerMenu() {
+    
+}
+
