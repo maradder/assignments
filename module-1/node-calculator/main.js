01c51fe4eaad7d75bb372c9ff6cbd1c4054a85fc
Marcus Radder <marcusradder@gmail.com>
2021-03-28 16:13:09 -0700

    Committed node-calculator -WORKING VERSION- and established a directory for Mario with an image that I plan to use.

---
+++ module-1/node-calculator/main.js
@@ -0,0 +1,60 @@
+const readlineSync = require("readline-sync")
+
+
+// Each function will have 2 parameters, num1, num2
+// A function that adds two numbers and returns the result
+function sum2Num(num1, num2) {
+    return Number(num1) + Number(num2); // Had to add the 'Number' to the front of the variable for adding.  The other operations worked fine without it
+}
+
+// A function that multiplies two numbers and returns the result
+function prod2Num(num1, num2) {
+    return num1 * num2;
+}
+
+// A function that divides two numbers and returns the result
+function div2Num(num1, num2) {
+    return num1 / num2;
+}
+
+// A function that subtracts two numbers and returns the result
+function diff2Num(num1, num2) {
+    return num1 - num2;
+}
+
+
+
+// On the console the prompt will print to the user:
+// Please enter your first number (store that number)
+var num1 = readlineSync.question("Please enter your first number: ");
+
+// Please enter your second number (store that number)
+var num2 = readlineSync.question("Please enter your second number: ");
+
+// Please enter the operation to perform: add, sub, mul, div (then store the operation)
+var operation = readline
+
+
+
+
+var result = 0
+
+// Based on the operation entered by the user, you will call one of your 4 functions to perform the correct operation
+if (operation == "add") {
+    result = sum2Num(num1, num2); 
+}
+else if (operation == "sub") {
+    result = diff2Num(num1, num2); 
+}
+
+else if (operation == "mul") {
+    result = prod2Num(num1, num2); 
+}
+
+else if (operation == "div") {
+    result = div2Num(num1, num2); 
+}
+
+// You will then print to the console: The result is: [the result]
+console.log("The result is: " + result)
+
