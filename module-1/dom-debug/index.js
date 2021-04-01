e3a8c04d2cf646116ff9b9e69f06837b08b0f903
Marcus Radder <marcusradder@gmail.com>
2021-04-01 01:24:05 -0700

    Completed DOM-Debug

---
+++ module-1/dom-debug/index.js
@@ -0,0 +1,49 @@
+const addBtn = document.getElementById("add");
+
+addBtn.addEventListener("click", function(){
+
+    console.log("hello");
+
+    // create new divs within 'list'
+    const subItem = document.createElement("div");
+
+    //  
+    var subItemValue = document.getElementById("input");
+    subItem.innerText = subItemValue;
+    document.getElementById("list").appendChild(subItem);
+    const dropDown = createDropDown();
+    subItem.appendChild(dropDown);
+    subItem.setAttribute("class", "subItem");
+
+});
+
+function runIt() {
+    createDropDown();
+    createSubItem()
+}
+
+
+const main = document.querySelector(".main");
+const colors = ["red", "blue", "green"];
+
+
+function createDropDown(){
+    const dropDown = document.createElement("select");
+    main.appendChild(dropDown);
+    console.log('poop1');
+
+    for (var i = 0; i < colors.length; i++){
+        const option = document.createElement("option"); 
+        option.innerHTML = colors[i];
+        option.value = colors[i];
+        dropDown.appendChild(option);
+        console.log('poop2')
+    }
+    console.dir(dropDown)
+
+    return dropDown;
+}
+
+createDropDown();
+
+
